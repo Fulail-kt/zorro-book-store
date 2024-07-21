09e6c7d1c01a78990fc79932c17808e5786257c4
fulail-kt <muhamedfulail77@gmail.com>
2024-07-21 06:20:18 +0530

    env upated

--- frontend/src/service/firebase/firebase.config.js
+++ frontend/src/service/firebase/firebase.config.js
@@ -2,15 +2,21 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDFYhA41X5rkJyZ6uT-2DMxGdnKbUDJ-w4",
-    authDomain: "zorro-6f846.firebaseapp.com",
-    projectId: "zorro-6f846",
-    storageBucket: "zorro-6f846.appspot.com",
-    messagingSenderId: "1003431666301",
-    appId: "1:1003431666301:web:58c27cd69867ff18228714",
-    measurementId: "G-9VBEZ7HFH5"
+    apiKey:process.env.NEXT_API_KEY,
+    authDomain:process.env.NEXT_DOMAIN,
+    projectId:process.env.NEXT_PROJECT_ID,
+    storageBucket:process.env.NEXT_STORAGE_BUCKET,
+    messagingSenderId:process.env.NEXT_MESSAGING_SENDER_ID,
+    appId:process.env.NEXT_APP_ID,
+    measurementId:process.env.NEXT_MEASUREMENT_ID
   };
 
+  
+  
+
+
+
+ 
   
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
